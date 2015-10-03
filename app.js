7500ae2632d6237d611414edae2336b677e482d7
Micah Bell <micahmbell@gmail.com>
2015-10-03 16:22:09 -0600

    Angular and Sass wired

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').load();
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var glazes = require('./routes/glazes');
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/glazes', glazes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
